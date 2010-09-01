16aecdc0f1d5df65b784baa7c0a4eec0fa791cf8
jeremy.french <jeremy.french@French.tmg.co.uk>
2010-09-01 09:32:02 +0100

    Change to make work on different domains

--- js/diseaselookup.js
+++ js/diseaselookup.js
@@ -6,16 +6,16 @@ Drupal.behaviors.diseaselookup = {
 	jQuery("input#edit-search-field").autocomplete({
 	    source: function(request,response) {
           jQuery.ajax({
-        	url: "http://nic.frost.tmg.co.uk:8880/sparql/", // TODO get the url from a setting
+        	url: 'http://'+ location.host + '/sparql/',
         	dataType: "json",
         	
         	data: {
         	  query: "SELECT Distinct ?id  ?title WHERE { " +
+        	                       " ?id <http://www.w3.org/2004/02/skos/core#prefLabel> ?pref_title"+
         						   "{ ?id <http://www.w3.org/2004/02/skos/core#prefLabel> ?title }" +
         						   "UNION" +
-        	                  "{ ?id <http://www.w3.org/2004/02/skos/core#altLabel> ?title } "+
-   						      "UNION" +
-     	                      "{ ?id <http://www.w3.org/2004/02/skos/core#hiddenLabel> ?title } "+
+        	                  "{ ?id <http://www.w3.org/2004/02/skos/core#altLabel> ?title." +
+        	                  "   } "+
         	                  "?id <http://www.w3.org/2004/02/skos/core#broader> ?parent."+
         	                  "?parent <http://www.nlm.nih.gov/mesh/2006#runningHead> ?parent_head."+
         	                  "Filter Regex(?title,'"+request.term.toLowerCase()+"','i')."+
@@ -42,7 +42,7 @@ Drupal.behaviors.diseaselookup = {
 	    minLength: 2,
 	    delay: 50,
 	    select: function(event, ui) {
-     	 window.location.href = "http://nic.frost.tmg.co.uk:8880/disease/" + ui.item.value;
+     	 window.location.href = 'http://' + location.host + '/disease/' + ui.item.value;
          },
 	})}};
   
